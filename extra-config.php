5814fd395848a596a257d4e776bf657750de502a
Lanny Heidbreder <lanny@75thtrombone.com>
2020-06-21 21:58:48 -0500

    Removed swap file and added extra WordPress config

---
+++ extra-config.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * WordPress configuration that I don't want blown away on every deployment.
+ *
+ * This file lives in our public Git repo, so for the love of God, don't put any
+ * sensitive credentials in here!
+ */
