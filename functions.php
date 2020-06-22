6f87e62c5bdd0b0460e731b0b1f293ef1da6f965
Lanny Heidbreder <lanny@75thtrombone.com>
2020-06-21 21:49:52 -0500

    Ready for beta gameplay

--- functions.php
+++ functions.php
@@ -9,22 +9,45 @@
 
 // this code loads the parent's stylesheet (leave it in place unless you know what you're doing)
 
-function your_theme_enqueue_styles() {
+function blognomic_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style';
+  $parent_style = 'parent-style';
 
-    wp_enqueue_style( $parent_style, 
-      get_template_directory_uri() . '/style.css'); 
+  wp_enqueue_style( $parent_style,
+    get_template_directory_uri() . '/style.css');
 
-    wp_enqueue_style( 'child-style', 
-      get_stylesheet_directory_uri() . '/style.css', 
-      array($parent_style), 
-      wp_get_theme()->get('Version') 
-    );
+  wp_enqueue_style( 'child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array($parent_style),
+    wp_get_theme()->get('Version')
+  );
+
+  wp_enqueue_style(
+    'blognomic-theme-style',
+    get_stylesheet_directory_uri() . '/dist/css/blognomic.css',
+    ['child-style'],
+    wp_get_theme()->get('Version')
+  );
 }
 
-add_action('wp_enqueue_scripts', 'your_theme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'blognomic_theme_enqueue_styles');
 
-/*  Add your own functions below this line.
-    ======================================== */ 
+/*  Add your own functions below this line                                                                                                                                                                                                                                                                                                               .
+    ======================================== */
 
+// Default the "Admin User" field to the current user.
+function blognomic_theme_admin_user_default($field) {
+  if(empty($field['value'])) {
+    $field['value'] = get_current_user_id();
+  }
+
+  return $field;
+}
+add_filter('acf/load_field/name=admin_user', 'blognomic_theme_admin_user_default');
+
+
+function blognomic_theme_sidebar_pending_query_alter($query) {
+  $query->set('post_status', ['open', 'publish']);
+}
+
+add_action('elementor/query/sidebar-pending', 'blognomic_theme_sidebar_pending_query_alter');
--- template-parts/single.php
+++ template-parts/single.php
@@ -4,27 +4,40 @@
  *
  * @package HelloElementor
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
 ?>
-<?php
-while ( have_posts() ) : the_post();
+
+<?php if($post->post_type === 'post'): ?>
+  <?php
+		$context = Timber::get_context();
+		$post = Timber::query_post();
+		$context['post'] = $post;
+
+		if ( post_password_required( $post->ID ) ) {
+			Timber::render( 'single-password.twig', $context );
+		} else {
+			Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
+		}
 	?>
-
-<main <?php post_class( 'site-main' ); ?> role="main">
-	<div class="page-content">
-		<?php the_content(); ?>
-		<div class="post-tags">
-			<?php the_tags( '<span class="tag-links">' . __( 'Tagged ', 'hello-elementor' ), null, '</span>' ); ?>
-		</div>
-		<?php wp_link_pages(); ?>
-	</div>
-
-	<?php comments_template(); ?>
-</main>
-
+<?php else: ?>
 	<?php
-endwhile;
+		if ( ! defined( 'ABSPATH' ) ) {
+			exit; // Exit if accessed directly.
+		}
+	?>
+	<?php	while ( have_posts() ) : ?>
+		<?php the_post(); ?>
 
+		<main <?php post_class( 'site-main' ); ?> role="main">
+			<div class="page-content">
+				<?php the_content(); ?>
+				<div class="post-tags">
+					<?php the_tags( '<span class="tag-links">' . __( 'Tagged ', 'hello-elementor' ), null, '</span>' ); ?>
+				</div>
+				<?php wp_link_pages(); ?>
+			</div>
+
+			<?php comments_template(); ?>
+		</main>
+
+	<?php	endwhile; ?>
+<?php endif; ?>
